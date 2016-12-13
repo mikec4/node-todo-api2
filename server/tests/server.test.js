afb7f8521d3bb61e946114b460234e0d0388efff
ntiriniga <clarantiriniga@gmail.com>
2016-12-13 14:50:31 +0300

    PATCH /todo/:id test

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -11,10 +11,13 @@ const {Todo}=require('./../models/todo');
 const todos=[{
   _id:new ObjectID,
   text:'Hello kido'
+
 },
 {
   _id:new ObjectID(),
-  text:"maharage"
+  text:"maharage",
+    completed:true,
+  completedAt:333
 },
 {  
   _id:new ObjectID(),
@@ -184,3 +187,48 @@ beforeEach((done)=>{
  })
 
  });
+
+
+describe('PATCH /todoz/:id',()=>{
+  
+  it('should update the todo',(done)=>{
+   
+    var id=todos[0]._id;
+     var text="This is my update";
+
+    request(app)
+    .patch(`/todoz/${id}`)
+    .send({text,
+      completed:true
+    })
+    .expect(200)
+    .expect((res)=>{
+       expect(res.body.todo.text).toBe(text);
+      expect(res.body.todo.completed).toBe('true');
+       expect(res.body.todo.completedAt).toBeA('number');
+
+    })
+    .end(done);
+  });
+
+  it('should clear completedAt when todo is not completed',(done)=>{
+       var id=todos[1]._id;
+     var text="This is my world";
+
+    request(app)
+    .patch(`/todoz/${id}`)
+    .send({text,
+      completed:false
+    })
+    .expect(200)
+    .expect((res)=>{
+      expect(res.body.todo.text).toBe(text);
+      expect(res.body.todo.completed).toBe('false');
+      expect(res.body.todo.completedAt).toNotExist();
+
+    })
+    .end(done); 
+
+  });
+  
+});
